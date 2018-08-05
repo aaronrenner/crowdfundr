65d79df4964225c2672f31e9bb406333dc1c3c8d
Aaron Renner <aaron.renner@gmail.com>
2018-08-04 21:08:27 -0600

    Remove unused functions from Accounts module

--- apps/crowdfundr/lib/crowdfundr/accounts/accounts.ex
+++ apps/crowdfundr/lib/crowdfundr/accounts/accounts.ex
@@ -7,19 +7,6 @@ defmodule Crowdfundr.Accounts do
   alias Crowdfundr.Accounts.User
 
   @doc """
-  Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
-  """
-  def list_users do
-    Repo.all(User)
-  end
-
-  @doc """
   Gets a single user.
 
   Raises `Ecto.NoResultsError` if the User does not exist.
@@ -51,52 +38,5 @@ defmodule Crowdfundr.Accounts do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_user(%User{} = user, attrs) do
-    user
-    |> User.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a User.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_user(%User{} = user) do
-    Repo.delete(user)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{source: %User{}}
-
-  """
-  def change_user(%User{} = user) do
-    User.changeset(user, %{})
   end
 end
--- apps/crowdfundr/test/crowdfundr/accounts/accounts_test.exs
+++ apps/crowdfundr/test/crowdfundr/accounts/accounts_test.exs
@@ -8,7 +8,6 @@ defmodule Crowdfundr.AccountsTest do
     alias Crowdfundr.Accounts.User
 
     @valid_attrs %{email: "some email", password: "some password"}
-    @update_attrs %{email: "some updated email", password: "some updated password"}
     @invalid_attrs %{email: nil, password: nil}
 
     def user_fixture(attrs \\ %{}) do
@@ -18,11 +17,6 @@ defmodule Crowdfundr.AccountsTest do
         |> Accounts.create_user()
 
       user
-    end
-
-    test "list_users/0 returns all users" do
-      %User{id: user_id} = user_fixture()
-      assert [%User{id: ^user_id}] = Accounts.list_users()
     end
 
     test "get_user!/1 returns the user with given id" do
@@ -38,31 +32,6 @@ defmodule Crowdfundr.AccountsTest do
 
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
-    end
-
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, user} = Accounts.update_user(user, @update_attrs)
-      assert %User{} = user
-      assert user.email == "some updated email"
-      assert Argon2.checkpw("some updated password", user.password_hash)
-    end
-
-    test "update_user/2 with invalid data returns error changeset" do
-      %User{id: user_id} = user = user_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert %User{id: ^user_id} = Accounts.get_user!(user_id)
-    end
-
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
-      assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
-    end
-
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
 end
